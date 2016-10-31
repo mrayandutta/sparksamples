f8b2db4343c26e8e99430584620728d9f07926e0
Ayan Dutta <mrayandutta@gmail.com>
2016-10-31 08:22:41 +0530

    Updated

--- src/main/scala/sparksamples/basics/SparkPi.scala
+++ src/main/scala/sparksamples/basics/SparkPi.scala
@@ -5,12 +5,14 @@ import org.apache.spark._
 import sparksamples.common.EnvironmentConstants
 
 /** Computes an approximation to pi */
-object SparkPi {
-  def main(args: Array[String]) {
+object SparkPi
+{
+  def main(args: Array[String])
+  {
     //val sc = new SparkContext( "local", "Word Count", EnvironmentConstants.ScalaInstallationLocation, Nil, Map(), Map())
-    val sc = new SparkContext( "local", "Word Count", System.getenv("SPARK_HOME"), Nil, Map(), Map())
+    val sc = new SparkContext( "local", "SparkPi", System.getenv("SPARK_HOME"), Nil, Map(), Map())
     //System.getenv("SPARK_HOME")
-    val slices = if (args.length > 0) args(0).toInt else 2
+    val slices = 2
     val n = 100000 * slices 
     val count = sc.parallelize(1 to n, slices).map { i =>
       val x = random * 2 - 1
